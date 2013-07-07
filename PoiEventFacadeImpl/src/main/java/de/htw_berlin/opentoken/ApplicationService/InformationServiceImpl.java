55600e9ec06d063248f1253a7ffb83fe2d7302e8
Jakob Warnow <jakob.warnow@student.HTW-Berlin.de>
2013-07-07 21:12:06 +0200

    InformationServiceImpl

--- PoiEventFacadeImpl/src/main/java/de/htw_berlin/opentoken/ApplicationService/InformationServiceImpl.java
+++ PoiEventFacadeImpl/src/main/java/de/htw_berlin/opentoken/ApplicationService/InformationServiceImpl.java
@@ -11,4 +11,58 @@ import de.htw_berlin.f4.ai.kbe.poievent.Message;
 @Service
 public class InformationServiceImpl implements InformationService {
 
+	@Override
+	public Event erstelleEvent(Long userId, String poiName, String title) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Long istBesitzerVon(Long userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void loescheEvent(Long userId, Long eventId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Set<Event> erstellePoiListe(String poiName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean validateEvent(Long eventId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void addUserToEvent(Long userId, Long eventId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Set<Event> findSubscribedEventsBy(Long userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<Event> findOwnedEventsBy(Long userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Message> getMessage(Long eventId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
