a9a7a077e228266752637550276ef73665b5215e
Jakob Warnow <jakob.warnow@student.HTW-Berlin.de>
2013-07-07 21:15:30 +0200

    Weitere Konflickte gelöst

--- PoiEventFacadeImpl/src/main/java/de/htw_berlin/opentoken/ApplicationService/PoiServiceImpl.java
+++ PoiEventFacadeImpl/src/main/java/de/htw_berlin/opentoken/ApplicationService/PoiServiceImpl.java
@@ -2,4 +2,10 @@ package de.htw_berlin.opentoken.ApplicationService;
 
 public class PoiServiceImpl implements PoiService {
 
+	@Override
+	public boolean validatePoi(String poiName) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
 }
