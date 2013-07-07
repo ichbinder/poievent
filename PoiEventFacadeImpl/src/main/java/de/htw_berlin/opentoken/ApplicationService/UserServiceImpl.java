ea3bdc0c36c1aa68e505b405aa59670404e62020
Jakob Warnow <jakob.warnow@student.HTW-Berlin.de>
2013-07-07 21:17:44 +0200

    User konflickte angepasst

--- PoiEventFacadeImpl/src/main/java/de/htw_berlin/opentoken/ApplicationService/UserServiceImpl.java
+++ PoiEventFacadeImpl/src/main/java/de/htw_berlin/opentoken/ApplicationService/UserServiceImpl.java
@@ -5,4 +5,10 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserServiceImpl implements UserService {
 
+	@Override
+	public boolean validateUser(Long userId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
 }
